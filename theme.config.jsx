934c930047d1d4c12ce34f61ad66abc6c6035f9d
masonjnguyen <116712800+masonjnguyen@users.noreply.github.com>
2023-03-02 17:14:12 +0700

    feat: add contents

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,19 @@
 export default {
-    logo: <span>My Nextra Documentation</span>,
+    logo: <span style={{fontWeight: 700}}>UX Hub</span>,
     project: {
-      link: 'https://github.com/shuding/nextra',
+      link: 'https://github.com/masonjnguyen/uxdocs',
     },
-    // ...
+    docsRepositoryBase: 'https://github.com/masonjnguyen/uxdocs/tree/master',
+    editLink: {
+      text: 'Edit this page →'
+    },
+    footer: {
+      text: <span>
+        Gathered from <a href="https://grow.google/certificates/ux-design">the Google UX Design Certificate</a> by <a href="https://masonjnguyen.com" target="_blank">masonjnguyen.com</a>
+      </span>,
+    },
+    feedback: {
+      content: null,
+    }
+    
   }
